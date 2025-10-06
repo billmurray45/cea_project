2c62f1e53e8dc88775d0f06e67c9b5323170d72f
Bauyrzhan <billfookingmurray@gmail.com>
2025-10-06 20:48:44 +0500

    fix etc

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.users.routes import router as users_router
 
 app = FastAPI(
     title="CEA YU Project",
@@ -8,10 +13,21 @@ app = FastAPI(
     version="1.0",
 )
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.include_router(users_router)
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    """ Главная Landing страница """
+    return templates.TemplateResponse(
+        "landing.html",
+        {
+            "request": request
+        }
+    )
 
 
 if __name__ == "__main__":
